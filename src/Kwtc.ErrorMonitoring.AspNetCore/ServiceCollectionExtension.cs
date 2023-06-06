e2665508385334dbdac42a2dcfeb259f9216394b
Kasper Christiansen <kasper@kwc.dk>
2023-06-06 06:49:43 +0200

    Registered api client

--- src/Kwtc.ErrorMonitoring.AspNetCore/ServiceCollectionExtension.cs
+++ src/Kwtc.ErrorMonitoring.AspNetCore/ServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
 namespace Kwtc.ErrorMonitoring.AspNetCore;
 
+using Client;
 using Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtension
@@ -7,6 +8,8 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddErrorMonitoring(this IServiceCollection services)
     {
         // TODO: Add the ErrorMonitoringMiddleware to the pipeline
+
+        services.AddScoped<IApiClient, ApiClient>();
 
         return services;
     }
