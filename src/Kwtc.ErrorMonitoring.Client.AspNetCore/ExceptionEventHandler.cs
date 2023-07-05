348c39d6f67e42c742fbdc2516e37ac6b1b13629
Kasper Christiansen <kasper@kwc.dk>
2023-07-05 06:08:52 +0200

    Added hosted service and dependencies

---
+++ src/Kwtc.ErrorMonitoring.Client.AspNetCore/ExceptionEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+using Microsoft.Extensions.Hosting;
+
+namespace Kwtc.ErrorMonitoring.Client.AspNetCore;
+
+public class ExceptionEventHandler : IHostedService
+{
+    private readonly Channel<ExceptionEvent> channel;
+
+    public ExceptionEventHandler(Channel<ExceptionEvent> channel)
+    {
+        this.channel = channel;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
